02632d32d6dddd9ba349f330f67fa33594279713
unknown <ojitdebnathfreelancer.com>
2024-01-23 12:46:58 +0600

    full project done

--- src/components/my-tree/TreeChild.jsx
+++ src/components/my-tree/TreeChild.jsx
@@ -1,4 +1,8 @@
 /* eslint-disable react/prop-types */
+import curve1 from "../../assets/curve1.png";
+import curve2 from "../../assets/curve2.png";
+import plus from "../../assets/plus.png";
+
 const TreeChild = ({ data }) => {
   return (
     <>
@@ -16,10 +20,19 @@ const TreeChild = ({ data }) => {
         </div>
       )}
 
+      {/* <div>hello</div> */}
+
       {data?.child && (
         <div className="child-container">
           {data.child.map((c, index) => (
             <div key={index} className="sub-tree">
+              {c?.curve && (
+                <div className="curve">
+                  <img src={curve1} alt="curve1" className="curve1" />
+                  <img src={plus} alt="plus button" className="plus-button" />
+                  <img src={curve2} alt="curve2" className="curve2" />
+                </div>
+              )}
               <TreeChild data={c} />
             </div>
           ))}
